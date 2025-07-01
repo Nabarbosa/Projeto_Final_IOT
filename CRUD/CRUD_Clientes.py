423c8b83d947083c51ae8b6e0e0497034cbf2d88
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-07-01 00:20:12 -0300

    Update CRUD_Clientes.py

--- CRUD/CRUD_Clientes.py
+++ CRUD/CRUD_Clientes.py
@@ -1,3 +1,10 @@
+"""
+Nome do arquivo: CRUD_Clientes.py
+Equipe: Clara, Rayanne e Tainá
+Turma: G91164
+Semestre: 2025.1
+"""
+
 # Importações necessárias
 from CRUD_Endereço import create_endereco
 from CRUD_Contatos import create_contato
