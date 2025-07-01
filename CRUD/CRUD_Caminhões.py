09a5f125bea02d0e435c0272d85027e2022cbad8
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-07-01 00:18:57 -0300

    Update CRUD_Caminhões.py

--- CRUD/CRUD_Caminhões.py
+++ CRUD/CRUD_Caminhões.py
@@ -1,3 +1,10 @@
+"""
+Nome do arquivo: CRUD_Caminhões
+Equipe: Clara, Rayanne e Tainá
+Turma: G91164
+Semestre: 2025.1
+"""
+
 # CRUD em arquivos de texto para gerenciar caminhões com informações completas
 
 # Função para gerar o próximo ID automaticamente
